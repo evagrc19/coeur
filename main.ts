41f9e5f8e274f3b6ccf680f433d07d46f83534b5
evagrc19 <103173881+evagrc19@users.noreply.github.com>
2022-04-19 13:24:36 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Heart)
